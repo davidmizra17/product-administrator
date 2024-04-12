a8d177667571e7930d4c5d82708b42013ec31931
davidmizra17 <david.mizrahi@correo.unimet.edu.ve>
2024-04-11 20:48:09 -0400

    Pruebas varias

--- src/handlers/__test__/product.test.ts
+++ src/handlers/__test__/product.test.ts
@@ -3,12 +3,38 @@ import server from '../../server'
 
 
 describe('POST /api,products', () => {
+
+    it('Price should be > 0', async () => {
+        const response = await request(server).post('/api/products').send({
+            name: "Mouse Ergonomico",
+            price: 0
+        })
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+    })
+    
+    it('Price should bea number and > 0', async () => {
+        const response = await request(server).post('/api/products').send({
+            name: "Mouse Ergonomico",
+            price: "hello"
+        })
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(2)
+    })
+
+
     it('Should display validation errors', async () => {
         const response = await request(server).post('/api/products').send({})
         expect(response.status).toBe(400)
         expect(response.body).toHaveProperty('errors')
         expect(response.body.errors).toHaveLength(4)
     })
+
+
     it('should create a new product', async () => {
         const response = await request(server).post('/api/products').send({
             name: "Mouse - Testing",
